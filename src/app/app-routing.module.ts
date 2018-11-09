9eb1128f1f718ad073abc5d61f2a4057816dc0d2
Lera <karabaza.lera@yandex.ru>
2018-11-10 02:08:54 +0300

    listsketch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from './home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'search', component: SearchComponent},
-  { path: '**', redirectTo: '/'}
+ { path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
