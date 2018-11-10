2d669334201e1572f8ef669c0a7209a9d58a2be5
Karabaza <karabaza.vlada@gmail.com>
2018-11-10 15:12:04 +0300

    final changes of the 1st page

--- src/app/video-form/video-form.component.ts
+++ src/app/video-form/video-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-video-form',
@@ -10,6 +11,30 @@ export class VideoFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $("#bank").on("change", function(){
+        $(this).css("color", "#000");
+      });
+    });
+
+    $(document).ready(function(){
+      $("#mesto").on("change", function(){
+        $(this).css("color", "#000");
+      });
+    });
+
+    $(document).ready(function(){
+      $("#dist").on("change", function(){
+        $(this).css("color", "#000");
+      });
+    });
+
+    $(document).ready(function(){
+      $("#technic").on("change", function(){
+        $(this).css("color", "#000");
+      });
+    });
   }
+
 
 }
